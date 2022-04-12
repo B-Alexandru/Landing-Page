7681f6f3ac59a38d7ed9e0c7bf5c386a32cb3742
Alexandru <64655529+B-Alexandru@users.noreply.github.com>
2022-04-12 16:45:13 +0100

    React Landing page

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Header from './components/Header';
+import Section from './components/Section';
+import Info from './components/Info';
+import Testimonials from './components/Testimonials';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Section/>
+      <Info/>
+      <Testimonials/>
+    </div>
+  );
+}
+
+export default App;
