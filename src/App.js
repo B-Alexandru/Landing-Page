b786d7f2c5e2ecab731cf1f3b62c659f7256e435
Alexandru <64655529+B-Alexandru@users.noreply.github.com>
2022-04-12 17:03:20 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import './App.css';
 import Header from './components/Header';
 import Section from './components/Section';
-import Info from './components/Info';
-import Testimonials from './components/Testimonials';
+
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Section/>
-      <Info/>
-      <Testimonials/>
+      
     </div>
   );
 }
